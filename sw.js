596fa52e9a5681cca6859af24d709fb58a4881dd
Trentan Healey <trentanh@gmail.com>
2026-04-21 20:59:59 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,27 @@
-const CACHE_NAME = 'travel-planner-v1';
+const CACHE_NAME = 'travel-planner-v2';
 const ASSETS = [
-  '.index.html',
-  '.manifest.json'
+  './index.html',
+  './manifest.json'
 ];
 
-self.addEventListener('install', (e) = {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) = cache.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) = {
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) = response  fetch(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
