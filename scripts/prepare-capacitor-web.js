73ecde08ba28b555aa7360ed72062689e0c76ad0
Trentan Healey <trentanh@gmail.com>
2026-08-19 09:06:34 +1000

    fix(mobile): hide top readOnlyBanner in mobile mode and use GoogleAuthUtil for token access

--- scripts/prepare-capacitor-web.js
+++ scripts/prepare-capacitor-web.js
@@ -45,11 +45,46 @@ if (fs.existsSync(googleAuthJava)) {
       'public void signIn(PluginCall call) {\n    if (googleSignInClient == null) {\n      String defaultClientId = this.getContext().getString(R.string.server_client_id);\n      String[] defaultScopes = new String[]{"profile", "email", "https://www.googleapis.com/auth/drive.file"};\n      loadSignInClient(defaultClientId, false, defaultScopes);\n    }'
     );
   }
-  // Ensure AccountManager mints an access token with Google Drive scope
-  javaContent = javaContent.replace(
-    '"oauth2:profile email"',
-    '"oauth2:https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.appdata profile email"'
-  );
+
+  // Replace getAuthToken with robust GoogleAuthUtil token retriever
+  const oldGetAuthTokenMethod = `private JSONObject getAuthToken(Account account, boolean retry) throws Exception {`;
+  if (javaContent.includes(oldGetAuthTokenMethod)) {
+    const startIdx = javaContent.indexOf(oldGetAuthTokenMethod);
+    const endIdx = javaContent.indexOf('private static String fromStream', startIdx);
+    if (startIdx !== -1 && endIdx !== -1) {
+      const replacementTokenCode = `private JSONObject getAuthToken(Account account, boolean retry) throws Exception {
+    String scope = "oauth2:https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.appdata profile email";
+    String authToken = null;
+    try {
+      authToken = com.google.android.gms.auth.GoogleAuthUtil.getToken(this.getContext(), account, scope);
+    } catch (Exception e) {
+      Log.w("GoogleAuth", "GoogleAuthUtil getToken exception: " + e.getMessage());
+      try {
+        AccountManager manager = AccountManager.get(this.getContext());
+        AccountManagerFuture<Bundle> future = manager.getAuthToken(account, scope, null, this.getActivity(), null, null);
+        Bundle bundle = future.getResult();
+        authToken = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+      } catch (Exception e2) {
+        Log.e("GoogleAuth", "AccountManager getAuthToken exception: " + e2.getMessage());
+        throw e2;
+      }
+    }
+    if (authToken == null || authToken.isEmpty()) {
+      throw new Exception("Unable to retrieve Google Drive OAuth access token.");
+    }
+    JSONObject jsonResponse = new JSONObject();
+    jsonResponse.put(FIELD_ACCESS_TOKEN, authToken);
+    jsonResponse.put(FIELD_TOKEN_EXPIRES_IN, 3600);
+    jsonResponse.put(FIELD_TOKEN_EXPIRES, 3600 + (System.currentTimeMillis() / 1000));
+    return jsonResponse;
+  }
+
+  `;
+      javaContent = javaContent.substring(0, startIdx) + replacementTokenCode + javaContent.substring(endIdx);
+    }
+  }
+
   fs.writeFileSync(googleAuthJava, javaContent, 'utf8');
 }
+
 
